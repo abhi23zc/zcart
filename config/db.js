946f4f0a54339aa6a6e576f9e8a96acaba3f4b0c
abhi23zc <63635289+abhi23zc@users.noreply.github.com>
2024-01-03 21:10:47 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 // const URI = "mongodb://127.0.0.1:27017/zcart"
-const URI = "mongodb+srv://zrfx:0000@zcart.vvo21ph.mongodb.net/?retryWrites=true&w=majority"
+const URI = "mongodb+srv://zrf:0000@cluster0.1bp1paj.mongodb.net/?retryWrites=true&w=majority"
 
 import {v2 as cloudinary} from 'cloudinary';
           
